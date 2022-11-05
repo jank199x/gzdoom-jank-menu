118470f8c55463d4b8bf1de004c13c4009feb01d
Jank <jank@panichev.xyz>
2022-11-05 15:09:52 +0400

    add play count and last played

--- src/menu.py
+++ src/menu.py
@@ -3,6 +3,8 @@
 import configparser
 import os
 import sys
+from datetime import datetime
+from collections import namedtuple
 
 import questionary
 
@@ -10,6 +12,9 @@ import jank
 
 DEFAULTS_CONFIG = "defaults.ini"
 PATHS_CONFIG = "paths.ini"
+STATS_FILE = "stats.ini"
+
+Choice = namedtuple("Choice", ["name", "last_played", "play_count"])
 
 
 def launch():
@@ -20,11 +25,42 @@ def launch():
     config.read(f"{cwd}/{PATHS_CONFIG}")
     config.read(f"{cwd}/{DEFAULTS_CONFIG}")
 
+    stats = configparser.ConfigParser()
+    if os.path.exists(STATS_FILE):
+        stats.read(STATS_FILE)
+
     rootdir = config["paths"]["root"]
     launcherdir = f"{rootdir}/{config['dirnames']['launchers']}"
 
     launchermap = jank.launchermap(launcherdir)
-    choices = sorted(launchermap.keys())
+    choices_names = sorted(launchermap.keys())
+
+    choices_raw = []
+    for name in choices_names:
+        if name in stats:
+            last_played = stats[name]["last_played"]
+            play_count = stats[name]["play_count"]
+            choices_raw.append(
+                Choice(
+                    name=name,
+                    play_count=play_count,
+                    last_played=last_played,
+                )
+            )
+        else:
+            choices_raw.append(
+                Choice(
+                    name=name,
+                    play_count="0",
+                    last_played="not played",
+                )
+            )
+
+    choices_raw.sort(key=lambda x: (x.play_count, x.last_played), reverse=True)
+
+    choices = []
+    for choice in choices_raw:
+        choices.append(f"{choice.name}: {choice.last_played}, {choice.play_count}")
 
     selection = questionary.select(
         "Which Doom WAD?",
@@ -34,11 +70,27 @@ def launch():
     if not selection:
         sys.exit()
 
-    config.read(launchermap[selection])
+    wad_name = selection.split(":")[0]
+
+    config.read(launchermap[wad_name])
     launch_string = jank.makelaunchstring(
         cwd=rootdir,
         configfile=config,
     )
+
+    if wad_name not in stats:
+        stats[wad_name] = {
+            "last_played": datetime.now().date().isoformat(),
+            "play_count": 1,
+        }
+    else:
+        play_count = stats[wad_name]["play_count"]
+        stats[wad_name] = {
+            "last_played": datetime.now().date().isoformat(),
+            "play_count": int(play_count) + 1,
+        }
+    with open(STATS_FILE, "w+") as stats_file:
+        stats.write(stats_file)
 
     os.system(launch_string)
 
